5acff7bf6c998e726004018758604e2675763910
Paul Schwin <schwin42@gmail.com>
2017-07-29 21:00:42 -0700

    Improved comments and syntax for NArmedBandit

--- GlennPython/NArmedBandit.py
+++ GlennPython/NArmedBandit.py
@@ -3,7 +3,7 @@ import numpy as np
 
 #List out our bandits.  Currently bandit 4 (index#3) is set to most often
 #provide a positive reward.
-bandits = [0.2, 4, 0.5, 3, 1]
+bandits = [-5, -3, 0, 3, 5]
 num_bandits = len(bandits)
 def pullBandit(bandit):
 	#Get a random number.
@@ -27,7 +27,7 @@ chosen_action = tf.argmax(weights, 0)
 #to compute the loss, and use it to update the network.
 reward_holder = tf.placeholder(shape = [1], dtype = tf.float32)
 action_holder = tf.placeholder(shape = [1], dtype = tf.int32) #index of slot machine to choose
-responsible_weight = tf.slice(weights,action_holder,[1])
+responsible_weight = tf.slice(weights, action_holder, [1]) #scalar weight responsible for reward
 loss = -(tf.log(responsible_weight) * reward_holder)
 optimizer = tf.train.GradientDescentOptimizer(learning_rate = 0.001)
 update = optimizer.minimize(loss)
@@ -35,35 +35,34 @@ update = optimizer.minimize(loss)
 total_episodes = 2000 #Set total number of episodes to train agent on.
 total_reward = np.zeros(num_bandits) #Set scoreboard for bandits to 0.
 choices = np.zeros(num_bandits) #How many times each bandit was chosen
-e = 0.1 #Set the chance of taking a random action.
-#init = tf.initialize_all_variables() #DEPRECATED
+e = 0.1 #Set the chance of taking a random action (exploration rate)
+
 init = tf.global_variables_initializer()
 
 # Launch the tensorflow graph
 with tf.Session() as sess:
 	sess.run(init)
-	i = 0
-	while i < total_episodes:
+	for i in range(total_episodes):
 		
 		#Choose either a random action or one from our network.
 		if np.random.rand(1) < e:
-			action = np.random.randint(num_bandits)
+			action = np.random.randint(num_bandits) #Choose random action
 		else:
-			action = sess.run(chosen_action)
+			action = sess.run(chosen_action) #Choose action with highest weight
 		
 		reward = pullBandit(bandits[action]) #Get our reward from picking one of the bandits.
 		
 		#Update the network.
-		_,resp, ww = sess.run([update, responsible_weight,weights], feed_dict = { reward_holder: [reward], action_holder: [action] })
-		
+		_, old_weight, adjusted_weights = sess.run([update, responsible_weight,weights], feed_dict = { reward_holder: [reward], action_holder: [action] })
+		#print("resp = ", old_weight, "ww = ", adjusted_weights, "choice = ", action)
 		#Update our running tally of scores.
 		total_reward[action] += reward
 		choices[action] += 1
 		if i % 100 == 0:
 			print ("Running reward for the " + str(num_bandits) + " bandits: " + str(total_reward) + str(choices))
-		i+=1
-print ("The agent thinks bandit " + str(np.argmax(ww) + 1) + " is the most promising....")
-if np.argmax(ww) == np.argmax(-np.array(bandits)):
+		i += 1
+print ("The agent thinks bandit " + str(np.argmax(adjusted_weights) + 1) + " is the most promising....")
+if np.argmax(adjusted_weights) == np.argmax(-np.array(bandits)):
 	print ("...and it was right!")
 else:
 	print ("...and it was wrong!")
