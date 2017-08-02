fbc10fead80e252099f3941d0aaa9bf420747e1d
Paul Schwin <schwin42@gmail.com>
2017-08-02 01:13:29 -0700

    Added logging and annotation to CartPoler

--- CartPoler.py
+++ CartPoler.py
@@ -41,18 +41,18 @@ class Agent():
 			activation_fn = tf.nn.softmax,
 			biases_initializer = None
 			)
-		print("output", self.output)
-		self.chosen_action = tf.argmax(self.output, 1) #Initial action to choose
+		#print("output", self.output)
+		self.chosen_action = tf.argmax(self.output, 1) #action to choose
 		
 		#The next six lines establish the training procedure.
 		#We feed the reward and chosen action into the network
 		#to compute the loss, and use it to update the network.
-		self.reward_holder = tf.placeholder(shape = [None], dtype = tf.float32)
-		self.action_holder = tf.placeholder(shape = [None], dtype = tf.int32)
-		self.indexes = tf.range(0, #What is going on here?
+		self.reward_holder = tf.placeholder(shape = [None], dtype = tf.float32) #Holds time-indexed episode of discounted rewards
+		self.action_holder = tf.placeholder(shape = [None], dtype = tf.int32) #Holds time-indexed episode of actions
+		self.indexes = tf.range(0, 
 			tf.shape(self.output)[0]) * tf.shape(self.output)[1] + self.action_holder
-		print("indexes: ", self.indexes)
-		self.responsible_outputs = tf.gather(tf.reshape(self.output, [-1]), self.indexes)
+		#print("indexes: ", self.indexes)
+		self.responsible_outputs = tf.gather(tf.reshape(self.output, [-1]), self.indexes) #Outputs for all actions in the episode
 		self.loss = -tf.reduce_mean(tf.log(self.responsible_outputs) * self.reward_holder)
 		
 		tvars = tf.trainable_variables()
@@ -98,8 +98,9 @@ with tf.Session() as session:
 			a_dist = session.run(agent.output, feed_dict = {
 				agent.state_in:[s]
 				})
-			a = np.random.choice(a_dist[0], p = a_dist[0])
-			a = np.argmax(a_dist == a)
+			print("a_dist", a_dist)
+			a = np.random.choice(a_dist[0], p = a_dist[0]) #Choose action with probability proportional to output weight
+			a = np.argmax(a_dist == a) #chosen action
 			
 			s1, r, d, _ = environment.step(a) #Get our reward for taking an action given a bandit
 			ep_history.append([s, a, r, s1])
@@ -121,6 +122,7 @@ with tf.Session() as session:
 				#print("stacked: ", np.vstack(ep_history[:,0]))
 
 				grads = session.run(agent.gradient_holders, feed_dict = feed_dict)
+
 				for idx, grad in enumerate(grads):
 					grad_buffer[idx] += grad
 					
@@ -128,8 +130,7 @@ with tf.Session() as session:
 					feed_dict = dictionary = dict(zip(agent.gradient_holders, grad_buffer))
 					_ = session.run(agent.update_batch, feed_dict = feed_dict)
 					#print(agent.indexes.eval())
-
-					#print(_)
+					#print(session.run(agent.indexes, feed_dict = feed_dict))
 					for ix, grad in enumerate(grad_buffer):
 						grad_buffer[ix] = grad * 0
 						
@@ -138,12 +139,16 @@ with tf.Session() as session:
 				break
 			
 			
+		print("output: ", session.run(agent.output, feed_dict = feed_dict))
+		print("chosen action: ", session.run(agent.chosen_action, feed_dict = feed_dict))
+		print("actions: ", session.run(agent.action_holder, feed_dict = feed_dict))
+		print("indexes: ", session.run(agent.indexes, feed_dict = feed_dict))
+		print("responsible outputs: ", session.run(agent.responsible_outputs, feed_dict = feed_dict))
+
 		#Update our running tally of scores
 		if i % 100 == 0:
 			print("Average episode reward: " + str(np.mean(total_reward[-100:])))
 	
 
-		
-#print(discount_rewards(np.array([1., 6., 9., 0.])))
 
 #print("copasetic")
