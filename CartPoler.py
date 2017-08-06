e72d26ab1e4ea399535dbe223091cfb970ea4d42
Schwin <schwin42@Flan.local>
2017-08-05 19:22:42 -0700

    Added line for environment visualization, added annotation, removed some print lines

--- CartPoler.py
+++ CartPoler.py
@@ -51,17 +51,17 @@ class Agent():
 		self.action_holder = tf.placeholder(shape = [None], dtype = tf.int32) #Holds time-indexed episode of actions
 		self.indexes = tf.range(0, 
 			tf.shape(self.output)[0]) * tf.shape(self.output)[1] + self.action_holder #Within a flattened m x K dimensional matrix, the index responsible for each action taken
-		print("indexes: ", self.indexes)
 		self.responsible_outputs = tf.gather(tf.reshape(self.output, [-1]), self.indexes) #Outputs for all actions in the episode
 		self.loss = -tf.reduce_mean(tf.log(self.responsible_outputs) * self.reward_holder)
 		
-		tvars = tf.trainable_variables()
-		self.gradient_holders = []
-		for idx, var in enumerate(tvars):
-			placeholder = tf.placeholder(tf.float32, name = str(idx) + '_holder')
+		tvars = tf.trainable_variables() #Contains fully connected layer weights
+		self.gradient_holders = [] #Create empty list
+		#Add gradient placeholders for each fully connected layer
+		for fc_layer_index, _ in enumerate(tvars): #_ is actual variable
+			placeholder = tf.placeholder(tf.float32, name = str(fc_layer_index) + '_holder')
 			self.gradient_holders.append(placeholder)
 		
-		self.gradient_holders = tf.gradients(self.loss, tvars)
+		self.gradients = tf.gradients(self.loss, tvars)
 		
 		optimizer = tf.train.AdamOptimizer(learning_rate = alpha)
 		self.update_batch = optimizer.apply_gradients(zip(self.gradient_holders, tvars))
@@ -72,8 +72,8 @@ tf.reset_default_graph()
 agent = Agent(1e-2, 4, 2, 8) #Load the agent
 
 total_episodes = 5000 #Set total number of episodes to train agent on.
-max_ep = 999 #Max length of episode
-update_frequency = 5 #And this? Really? No comments??
+max_ep = 999 #Max length of episode (anything over 200 is unclamped, since that limit is built into the environment
+update_frequency = 5 #How often to backpropagate
 
 init = tf.global_variables_initializer()
 
@@ -86,8 +86,8 @@ with tf.Session() as session:
 	total_length = []
 	
 	grad_buffer = session.run(tf.trainable_variables())
-	for ix, grad in enumerate(grad_buffer):
-		grad_buffer[ix] = grad * 0
+	for index, grad in enumerate(grad_buffer): #Zero out grad buffer
+		grad_buffer[index] = grad * 0
 	
 	for i in range(total_episodes):
 		s = environment.reset()
@@ -98,57 +98,44 @@ with tf.Session() as session:
 			a_dist = session.run(agent.output, feed_dict = {
 				agent.state_in:[s]
 				})
-			print("a_dist", a_dist)
 			a = np.random.choice(a_dist[0], p = a_dist[0]) #Choose action with probability proportional to output weight
 			a = np.argmax(a_dist == a) #chosen action
 			
+			#environment.render()
 			s1, r, d, _ = environment.step(a) #Get our reward for taking an action given a bandit
 			ep_history.append([s, a, r, s1])
 			s = s1
 			running_reward += r
 			if d == True: #If episode is completed
 				#Update the network
-				#print("unarrayed", type(ep_history))
 				ep_history = np.array(ep_history)
-				#print("arrayed", type(ep_history))
 				ep_history[:,2] = discount_rewards(ep_history[:,2]) #Set rewards to discounted rewards
-				#print("unstacked: ", ep_history[:,0])
-				#print("reward", ep_history[:,1])
-				feed_dict = { 
+				feed_dict = {
 					agent.reward_holder: ep_history[:,2], #Total sequence of discounted rewards received in last episode
 					agent.action_holder: ep_history[:,1], #Total sequence of actions taken in last episode
 					agent.state_in: np.vstack(ep_history[:,0]) #Get all rows, state column
 					}
-				#print("stacked: ", np.vstack(ep_history[:,0]))
 
-				grads = session.run(agent.gradient_holders, feed_dict = feed_dict)
-
-				for idx, grad in enumerate(grads):
-					grad_buffer[idx] += grad
+				grads = session.run(agent.gradients, feed_dict = feed_dict) #Gradients for both layers, one for each weight
+				#grad buffer aggregates gradients for multiple episodes
+				for index, grad in enumerate(grads): #Index is FC layer number
+					#print("assigning grad buffer index at ", index)
+					grad_buffer[index] += grad
 					
 				if i % update_frequency == 0 and i != 0:
-					feed_dict = dictionary = dict(zip(agent.gradient_holders, grad_buffer))
+					feed_dict = dictionary = dict(zip(agent.gradient_holders, grad_buffer)) #Generate feed dict from gradient buffer
 					_ = session.run(agent.update_batch, feed_dict = feed_dict)
-					#print(agent.indexes.eval())
-					#print(session.run(agent.indexes, feed_dict = feed_dict))
-					for ix, grad in enumerate(grad_buffer):
-						grad_buffer[ix] = grad * 0
+					for index, grad in enumerate(grad_buffer): #Zero out grad buffer
+						grad_buffer[index] = grad * 0
 						
 				total_reward.append(running_reward)
 				total_length.append(j)
 				break
 			
-			
-		print("output: ", session.run(agent.output, feed_dict = feed_dict))
-		print("chosen action: ", session.run(agent.chosen_action, feed_dict = feed_dict))
-		print("actions: ", session.run(agent.action_holder, feed_dict = feed_dict))
-		print("indexes: ", session.run(agent.indexes, feed_dict = feed_dict))
-		print("responsible outputs: ", session.run(agent.responsible_outputs, feed_dict = feed_dict))
-
+		print("episode #" + str(i) + " reward: ", str(running_reward))
 		#Update our running tally of scores
 		if i % 100 == 0:
-			print("Average episode reward: " + str(np.mean(total_reward[-100:])))
+			print("Average episode reward as of episode #" + str(i) + ": " + str(np.mean(total_reward[-100:])))
+			if np.mean(total_reward[-100:]) > 195:
+				print("Cartpole solved!")
 	
-
-
-#print("copasetic")
